bb963d339e74748488412ef8fe4c822375c9bcd4
margareten <2771355@gmail.com>
2019-04-09 16:21:46 -0400

    condition if logged in - navbar

--- src/app/views/navbar/navbar.component.ts
+++ src/app/views/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  loggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  logout() {
+
+  }
 }
