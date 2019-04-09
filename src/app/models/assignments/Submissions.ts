4293de37b85a35a5bbb780dd3db395a9835f3de8
mosheperes <mosheper99@gmail.com>
2019-04-09 17:48:40 -0400

    added Submissions.ts And View-Assignments.ts

---
+++ src/app/models/assignments/Submissions.ts
@@ -0,0 +1,13 @@
+export class Submissions {
+  text: string;
+  studentId: number;
+  assignmentId: number;
+
+  constructor( text: string,
+  studentId: number,
+  assignmentId: number,){
+    this.text = text;
+    this.studentId = studentId;
+    this.assignmentId = assignmentId;
+  }
+}
